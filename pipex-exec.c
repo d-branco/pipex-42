72a3e871b762526d461d1790b6d47e14815b2e57
gris <185072671+d-branco@users.noreply.github.com>
2025-01-20 18:39:23 +0000

    norminette

--- pipex-exec.c
+++ pipex-exec.c
@@ -6,7 +6,7 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 07:54:14 by abessa-m          #+#    #+#             */
-/*   Updated: 2025/01/19 11:32:01 by abessa-m         ###   ########.fr       */
+/*   Updated: 2025/01/20 18:38:49 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,6 @@ void	execute_cmd(char **cmd_args, char **envp)
 		else
 		{
 			cmd_with_path = find_path_from_envp(cmd_args, envp);
-			ft_printf("About to exec\n\n"); //
 			execve(cmd_with_path, cmd_args, NULL);
 			perror("");
 		}
@@ -43,7 +42,7 @@ void	execute_cmd(char **cmd_args, char **envp)
 	else if (id == -1)
 		perror("Forking error.\n");
 	else
-		wait(NULL);
+		waitpid(id, NULL, 0);
 }
 
 static char	*find_path_from_envp(char **cmd_args, char **envp)
